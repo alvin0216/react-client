ceaa99ee6a84b6d06ba8a124d6aee41dc9702387
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-06-02 23:18:59 +0800

    add demo

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,14 @@
+import { useBearStore } from '../../store/use-bear-store';
+
 export const LoginPage: React.FC = () => {
+  const { bears, increase } = useBearStore();
   return (
     <div>
       <h1>Login</h1>
       <p>Welcome to the login page!</p>
+
+      <p>Bear: {bears}</p>
+      <button onClick={() => increase(2)}>increase</button>
     </div>
   );
 };
