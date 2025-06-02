6ceaef32655259a6830b5ae3247875875d5e52ea
Shaowei SW5 Guo <guosw5@lenovo.com>
2025-06-02 23:56:19 +0800

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
 import { LoginPage } from './pages/login';
+import { RegisterPage } from './pages/register';
+import { ResetPage } from './pages/reset';
 
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reset" element={<ResetPage />} />
+        <Route path="*" element={<p>404</p>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
